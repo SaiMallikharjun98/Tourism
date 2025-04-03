9b56bf600d656f6be5aab4bedc07d7fae22184e2
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-04-03 18:53:30 +0530

    text sizes changed

--- src/components/TouristPlace.jsx
+++ src/components/TouristPlace.jsx
@@ -10,7 +10,7 @@ const TouristPlace = (props) => {
         {props.places.map((place, index) => (
           <div key={index}>
             <div
-              className={`mt-5 mb-1 border-gray-400 border-[0.5px] rounded-lg w-24 flex flex-col text-center justify-center "
+              className={`mt-5 mb-1 border-gray-400 border-[0.5px] rounded-lg w-[200px] flex flex-col text-center justify-center "
             ${
               place.whenToVisit === "Summer" ? "bg-yellow-200" : "bg-blue-200"
             }`}
@@ -18,7 +18,7 @@ const TouristPlace = (props) => {
               <img
                 src={place.img}
                 alt={`${place.placeName} image`}
-                className="w-24 rounded-lg p-1"
+                className="w-[200px] rounded-lg p-1"
               />
               <h2 className="text-[8px] text-pink-400 ">{place.placeName}</h2>
               <p className="text-[5px] text-gray-400 ">{place.desc}</p>
