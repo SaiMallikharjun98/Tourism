f55d8085d89f3927ef93c33513b848c55caa19d6
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-04-03 18:52:40 +0530

    text sizes changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     {
       placeName: "Goa",
       desc: "Famous for its beaches, nightlife, and Portuguese heritage.",
-      img: "/public/Goa-image.avif",
+      img: "/Goa-image.avif",
       whenToVisit: "Winter",
       ratings: 4.5,
       price: 15000,
@@ -13,7 +13,7 @@ const App = () => {
     {
       placeName: "Manali",
       desc: "A high-altitude Himalayan resort town known for its cool climate and snow-capped mountains.",
-      img: "/public/manali-image.avif",
+      img: "/manali-image.avif",
       whenToVisit: "Summer",
       ratings: 4.7,
       price: 10000,
@@ -21,7 +21,7 @@ const App = () => {
     {
       placeName: "Kerala",
       desc: "Known for its palm-lined beaches, backwaters, and rich traditions.",
-      img: "/public/kerala-image.avif",
+      img: "/kerala-image.avif",
       whenToVisit: "Winter",
       ratings: 4.8,
       price: 20000,
@@ -29,7 +29,7 @@ const App = () => {
     {
       placeName: "Jaipur",
       desc: "The capital of Rajasthan, known for its historic palaces and vibrant culture.",
-      img: "/public/jaipur-image.avif",
+      img: "/jaipur-image.avif",
       whenToVisit: "Winter",
       ratings: 4.6,
       price: 12000,
--- src/components/TouristPlace.jsx
+++ src/components/TouristPlace.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const TouristPlace = (props) => {
   return (
     <>
-      <h1 className="text-2xl text-center text-blue-500 font-bold ">
+      <h1 className=" mt-10 text-5xl text-center text-blue-500 font-bold ">
         Tourism Places
       </h1>
-      <div className="flex flex-wrap justify-center gap-1">
+      <div className="flex flex-wrap justify-center gap-10">
         {props.places.map((place, index) => (
           <div key={index}>
             <div
-              className={`mt-1  mb-1 border-gray-400 border-[0.5px] rounded-lg w-[70px] flex flex-col text-center justify-center "
+              className={`mt-5 mb-1 border-gray-400 border-[0.5px] rounded-lg w-24 flex flex-col text-center justify-center "
             ${
               place.whenToVisit === "Summer" ? "bg-yellow-200" : "bg-blue-200"
             }`}
@@ -18,17 +18,17 @@ const TouristPlace = (props) => {
               <img
                 src={place.img}
                 alt={`${place.placeName} image`}
-                className="w-[90px] rounded-lg p-1"
+                className="w-24 rounded-lg p-1"
               />
-              <h2 className="text-[5px] text-pink-400 ">{place.placeName}</h2>
-              <p className="text-[3px] text-gray-400 ">{place.desc}</p>
-              <p className="text-[3px] text-gray-800 font-bold">
+              <h2 className="text-[8px] text-pink-400 ">{place.placeName}</h2>
+              <p className="text-[5px] text-gray-400 ">{place.desc}</p>
+              <p className="text-[5px] text-gray-800 font-bold">
                 Best time to visit: {place.whenToVisit}
               </p>
-              <p className="text-[2px] text-orange-500">
+              <p className="text-[3px] text-orange-500">
                 Ratings: {place.ratings}⭐
               </p>
-              <p className="text-[3px] text-black pb-2">
+              <p className="text-[4px] text-black pb-2">
                 Estimated Price: ₹{place.price}
               </p>
               {place.price < 12000 ? (
